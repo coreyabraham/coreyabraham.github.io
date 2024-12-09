05dc6825ade3d14e1ef03e15e51249cbff186ddb
Corey Abraham <corey.joe.abraham@gmail.com>
2024-12-09 14:20:10 +1100

    Introduced slideshow.css and slideshow.js files for content slideshows, Updated all Sub-Works Pages with newer Slideshow contents, Gutted `c_a_website.html` as it was unnecessary and doesn't relate to my career path all that much, Removed unnecessary double inclusions of Roboto Mono GoogleAPIs font importing call, Added numerous screenshots for almost all projects

---
+++ Assets/Scripts/slideshow.js
@@ -0,0 +1,34 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(index)
+{
+    showSlides(slideIndex += index);
+}
+
+function currentSlide(index)
+{
+    showSlides(slideIndex = index);
+}
+
+function showSlides(index)
+{
+    let i;
+
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (index > slides.length) { slideIndex = 1 }
+    if (index < 1) { slideIndex = slides.length }
+
+    for (i = 0; i < slides.length; i++) { 
+        slides[i].style.display = "none"; 
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active"
+}
